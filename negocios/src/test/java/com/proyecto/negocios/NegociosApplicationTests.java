2ab971fa842068e73fc0dea31b61e1e9254895f2
Yeinner Andrés Ascanio Ascanio <nioandrew15@gmail.com>
2025-10-03 16:31:48 -0500

    Cambios Finales

---
+++ negocios/src/test/java/com/proyecto/negocios/NegociosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.negocios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegociosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
